4d3de5d76000a943be8104953d80a2ec84416eeb
Stefan Hölzle <stefan.hoelzle@1und1.de>
2024-05-08 13:02:41 +0200

    WIP

--- test/features/step_definitions/common_steps.js
+++ test/features/step_definitions/common_steps.js
@@ -1,14 +1,24 @@
 const { When, Then, Given, Before, After } = require('@cucumber/cucumber')
 
-Before(function ()  {
+Before(function () {
   this.config = {}
+  this.loggerMock = {
+    messages: [],
+    log: function (message) {
+      this.messages.push(message)
+    }
+  }
+})
+After(function () {
+  console.log(this.loggerMock.messages.join('\n'))
+  this.attach(this.loggerMock.messages.join('\n'))
 })
 When('checker {string} with html page {string} is called', function (checkerName, htmlFilePath) {
   const Checker = require(`../../../lib/checker/${checkerName}`)
   const path = require('path')
   const HtmlPage = require('../../../lib/html/htmlPage')
   const htmlFile = path.parse(path.join(`${__dirname}/../../fixtures/html-files/`, htmlFilePath))
-  this.singleCheckResult = new Checker(this.config).performCheck(new HtmlPage(htmlFile.dir, htmlFile.base))
+  this.singleCheckResult = new Checker(this.config, this.loggerMock).performCheck(new HtmlPage(htmlFile.dir, htmlFile.base))
 })
 
 Given('config option {string} is {string}', function (optionName, optionValue) {
